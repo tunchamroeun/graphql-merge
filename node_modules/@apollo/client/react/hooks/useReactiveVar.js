2a9a891f074a4cb78f4b074bf8607ba654b3ec8c
Tun Cham Roeun <chamroeuntun25@outlook.com>
2020-09-21 23:29:36 +0700

    graphql update file structure

--- node_modules/@apollo/client/react/hooks/useReactiveVar.js
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from 'react';
-export function useReactiveVar(rv) {
-    var value = rv();
-    var mute = rv.onNextChange(useState(value)[1]);
-    useEffect(function () { return mute; }, []);
-    return value;
-}
-//# sourceMappingURL=useReactiveVar.js.map
